b50085e56fceb54dd909e10decd01caf4db0e997
Mark Allen Evans <president@worldvaporexpo.com>
2018-03-15 19:56:57 +0000

    stable on ubuntu 16.04

---
+++ kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/miner/crypto/c_blake256.c" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/crypto/c_blake256.c.o"
+  "/root/miner/crypto/c_groestl.c" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/crypto/c_groestl.c.o"
+  "/root/miner/crypto/c_jh.c" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/crypto/c_jh.c.o"
+  "/root/miner/crypto/c_keccak.c" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/crypto/c_keccak.c.o"
+  "/root/miner/crypto/c_skein.c" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/crypto/c_skein.c.o"
+  "/root/miner/crypto/soft_aes.c" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/crypto/soft_aes.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/miner/cli-miner.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/cli-miner.cpp.o"
+  "/root/miner/console.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/console.cpp.o"
+  "/root/miner/crypto/cryptonight_common.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/crypto/cryptonight_common.cpp.o"
+  "/root/miner/executor.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/executor.cpp.o"
+  "/root/miner/httpd.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/httpd.cpp.o"
+  "/root/miner/jconf.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/jconf.cpp.o"
+  "/root/miner/jpsock.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/jpsock.cpp.o"
+  "/root/miner/minethd.cpp" "/root/miner/kryp-stak-cpu/CMakeFiles/kryp-stak-cpu.dir/minethd.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
